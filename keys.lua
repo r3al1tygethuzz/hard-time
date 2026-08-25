ef3083f6226ad8bbdafbc013f10232047025c970
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-24 21:14:21 -0400

    key: generate 7017815829

--- keys.lua
+++ keys.lua
@@ -1,4 +1,8 @@
 local keys = {
+    ["7017815829"] = {
+        key = "MLY3-XL6M-4V88-96D9",
+        expires = 1790212454
+    },
     ["10734133556"] = {
         key = "HELLO",
         expires = 0
