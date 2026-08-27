d54cc4f28d3176fa2c690efa5650025cf99fb88f
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-27 18:42:52 -0400

    Clear all key entries from keys.lua
    Removed all key entries from the keys table.

--- keys.lua
+++ keys.lua
@@ -1,32 +1,4 @@
 local keys = {
-    ["123"] = {
-        key = "QA28-LESN-HBNJ-KMYR",
-        expires = 1787870903
-    },
-    ["23243"] = {
-        key = "test",
-        expires = 0
-    },
-    ["1730697990"] = {
-        key = "4BSE-YGNJ-Q9YG-VDHB",
-        expires = 2103206365
-    },
-    ["7017815829"] = {
-        key = "HEY",
-        expires = 0
-    },
-    ["10734133556"] = {
-        key = "HELLO",
-        expires = 0
-    },
-    ["11555262294"] = {
-        key = "IM_A_FEMBOY",
-        expires = 0
-    },
-    ["11570628921"] = {
-        key = "HIKIDS",
-        expires = 0
-    },
 }
 
 return keys
