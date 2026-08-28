af405d03b4c33e7071972fb66119242a3434f41a
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-27 23:20:55 -0400

    redeem D1PV-DCTM-K4YY for 5066822226

--- keys.lua
+++ keys.lua
@@ -3,6 +3,10 @@ local keys = {
         key = "2YRH-E8ST-T57A-2FLX",
         expires = 1787889661
     },
+    ["5066822226"] = {
+        key = "6BF9-HR8D-P6T3-6A88",
+        expires = 1787890848
+    },
 }
 
 return keys
