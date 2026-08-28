6aa085fdf1398fc68f33a2a03c8471f609aac35d
r3al1tygethuzz <pheydyn@gmail.com>
2026-08-28 06:41:50 -0400

    Automatic expired key cleanup

--- keys.lua
+++ keys.lua
@@ -1,20 +1,4 @@
 local keys = {
-    ["2757059608"] = {
-        key = "2YRH-E8ST-T57A-2FLX",
-        expires = 1787889661
-    },
-    ["5066822226"] = {
-        key = "6BF9-HR8D-P6T3-6A88",
-        expires = 1787890848
-    },
-    ["9459252735"] = {
-        key = "AUVT-KJLY-CJR3-ACVW",
-        expires = 1787890916
-    },
-    ["11584154312"] = {
-        key = "ZEUS",
-        expires = 1787891307
-    },
 }
 
 return keys
