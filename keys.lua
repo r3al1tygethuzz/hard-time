828e29db085fd3e639bd6fe0d0889982d5f0ef97
r3al1tygethuzz <pheydyn@gmail.com>
2026-09-13 15:08:31 -0400

    Delete unused key entries from keys.lua
    Removed multiple key entries from the keys table.

--- keys.lua
+++ keys.lua
@@ -3,34 +3,6 @@ local keys = {
         key = "dada",
         expires = 0
     },
-    ["9769920257"] = {
-        key = "tuffboi",
-        expires = 0
-    },
-    ["11555262294"] = {
-        key = "2P5P-V85D-HLYR-FXWQ",
-        expires = 1790633011
-    },
-    ["11591884598"] = {
-        key = "tuffboi",
-        expires = 0
-    },
-    ["11591988086"] = {
-        key = "rollex",
-        expires = 0
-    },
-    ["11592425232"] = {
-        key = "hey",
-        expires = 0
-    },
-    ["11598167952"] = {
-        key = "1234",
-        expires = 0
-    },
-    ["11600621225"] = {
-        key = "mtz5",
-        expires = 0
-    },
 }
 
 return keys
